9e8b627712dc9e57d59649518899f68ad6005302
Paulo Henrique <ph.equfpb@gmail.com>
2017-11-17 17:06:16 -0200

    Full

--- main.c
+++ main.c
@@ -1,25 +1,26 @@
-/// Neil, Paulo Henrique, Willian Abdon
+/// Neil John, Paulo Henrique, Willian Abdon
 
 #include <windows.h>
 #include <stdio.h>
 #include <conio.h>
 #include <time.h>
 #include "supermarket.h"
-#define SIMULATION_TIME 720
+
 
 
 int main(){
-    int timer, next, i;
+    int timer, next, i, boxTest = 2;
     char opt;
     tlista boxs;
 
     printf("-------------------------------------------------------------------------------------------------\n");
-    printf("                                    SUPERT MARKET SIMULATOR                                      \n");
+    printf("                                     SUPER MARKET SIMULATOR                                      \n");
     printf("-------------------------------------------------------------------------------------------------\n\n");
     printf("Quantidade de caixas                   [6]\n\n");
     printf("Tempo para chegada de novo cliente     [1 - 5]\n\n");
     printf("Quantidade de produtos                 [1 - 101]\n\n");
-    printf("Tempo de atendimento no caixa          [Produtos * 0.28]  (Arrendondado para cima) \n\n");
+    printf("Tempo de atendimento no caixa          [Produtos * %.2f]  (Arrendondado para cima)\n\n", PROC_PROD);
+    printf("Caixas 4 e 5 Inicialmente fechados, abertura quando necessario\n\n");
 
     printf("[ENTER] Start Simulation               [ESC] Exit\n");
     if((opt = getch()) == 27) return 0;
@@ -47,10 +48,10 @@ int main(){
             printf("   | Cx. 3 | "); imprimirFila(boxs, 4); printf("\n");
             printf("_________________________________________________________________________________________________\n\n");
             printf("_________________________________________________________________________________________________\n");
-            printf("   | Cx. 4 | "); imprimirFila(boxs, 5); printf("\n");
+            printf("   | Cx. 4 | "); (boxTest = aberturaCaixa(boxs, boxTest)) == 2 ? printf("FECHADO"): imprimirFila(boxs, 5); printf("\n");
             printf("_________________________________________________________________________________________________\n\n");
             printf("_________________________________________________________________________________________________\n");
-            printf("   | Cx. 5 | "); imprimirFila(boxs, 6); printf("\n");
+            printf("   | Cx. 5 | "); (boxTest = aberturaCaixa(boxs, boxTest)) >= 1 ? printf("FECHADO"): imprimirFila(boxs, 6); printf("\n");
             printf("_________________________________________________________________________________________________\n\n");
 
             countTime(&boxs);
